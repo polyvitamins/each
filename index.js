fc1619d6895f4b791c49e95270a565366ba373be
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-01-14 17:33:45 +0300

    supports array

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 	
 	module.exports = function(obj, fn, ctx) {
 		ctx = ctx || this;
-		for (var prop in obj) {
-			if (obj.hasOwnProperty(prop)) {
-				fn.call(ctx, obj[prop], prop);
+		if ("object"===typeof obj) {
+			if (obj instanceof Array) {
+				for (var i = 0;i<obj.length;++i) {
+					fn.call(ctx, obj[i], i);
+				}
+			} else {
+				for (var prop in obj) {
+					if (obj.hasOwnProperty(prop)) {
+						fn.call(ctx, obj[prop], prop);
+					}
+				}
 			}
+		} else {
+			console[console.hasOwnProperty('warn')?'warn':'log']('Argument[0] in `each` is not an object');
 		}
 		return obj;
 	}
